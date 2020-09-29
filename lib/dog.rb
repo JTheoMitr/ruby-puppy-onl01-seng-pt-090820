857078c6a484fee2e96e4a1ee76ff721956a28da
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-09-29 07:56:08 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,6 +5,19 @@ class Dog
   @@all = []
   
   def initialize(name)
-    @
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.print_all
+    @@all.each do |puppy|
+      puts puppy.name
+    end
+  end
+  
   
 end
